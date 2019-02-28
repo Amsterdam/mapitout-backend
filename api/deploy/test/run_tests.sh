c7d54dabd63635f513aecd57f1223141aa109c38
CBAmsterdam <info@iloki.nl>
2019-02-28 13:34:12 +0100

    pytest

--- api/deploy/test/run_tests.sh
+++ api/deploy/test/run_tests.sh
@@ -4,8 +4,6 @@ set -u   # crash on missing env variables
 set -e   # stop on any error
 set -x   # print what we are doing
 
-cd /app
-#source /deploy/docker-wait.sh
-#python manage.py test --noinput
+cd /deploy/test
 pytest -p no:cacheprovider
 
